0e024ab2aabc76f784082580d384b1dd5f1e2868
Armaan Gomes <acgo8888@gmail.com>
2024-08-02 08:08:38 -0700

    added files and updated project.v

--- src/project.v
+++ src/project.v
@@ -15,13 +15,27 @@ module tt_um_example (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
-
+wire rst;
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  // assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
-  assign uio_oe  = 0;
-
+  assign uio_oe[4:0]  = 0;
+  assign uio_oe[6:5]  = 1;
+  assign uio_oe[7]  = 0;
+  assign rst = !rst_n;
+  generate
+    supermic_top_module u_supermic_top_module (
+        .clk(ui_in[0]),
+        .rst(rst),
+        .lr_clk(ui_in[1]),
+        .delay_select(uio_in[4:0]),
+        .pdm(ui_in[5:2]),
+        .i2s_out(uio_out[5]),
+        .mic_clk(uio_out[6]),
+        .cic_out(uo_out)
+    );    
+  endgenerate
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, clk,ui_in[7:6],uio_in[7], 1'b0};
 
 endmodule
