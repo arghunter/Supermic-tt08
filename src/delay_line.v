e5ed2892454c75b3dd2bbdb70b772b70e65c5127
Armaan Gomes <91099806+arghunter@users.noreply.github.com>
2024-09-05 01:51:01 -0700

    Update delay_line.v

--- src/delay_line.v
+++ src/delay_line.v
@@ -8,12 +8,12 @@ module delay_line (
 
 parameter MAX_DELAY = 16;
 integer i;
-reg [4:0] counter = 0;
+
 reg [18:0] buffer [MAX_DELAY:0];
 
 always @(posedge clk or posedge rst) begin
 	if (rst) begin
-		counter <= 0;
+		
 		for (i = 0; i <= MAX_DELAY; i = i + 1) begin
 			buffer[i] <= 0;
 		end
