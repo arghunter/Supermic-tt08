2eab38811d5765acd13048d679d79e9d3f82f235
Armaan Gomes <91099806+arghunter@users.noreply.github.com>
2024-09-05 00:49:35 -0700

    Update delay_line.v

--- src/delay_line.v
+++ src/delay_line.v
@@ -1,14 +1,14 @@
 module delay_line (
 	input wire clk,
 	input wire rst,
-        input wire [3:0] delay,
+	input wire [4:0] delay,
 	input wire [18:0] pcm_data,
 	output wire [18:0] delayed_pcm_data
 );
 
 parameter MAX_DELAY = 16;
 integer i;
-reg [3:0] counter = 0;
+reg [4:0] counter = 0;
 reg [18:0] buffer [MAX_DELAY:0];
 
 always @(posedge clk or posedge rst) begin
