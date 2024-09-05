b97a40c19dbcf12a66a4222a3eeaadebfb1fbc9d
Armaan Gomes <91099806+arghunter@users.noreply.github.com>
2024-09-05 01:39:09 -0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -18,7 +18,7 @@ module tt_um_supermic_arghunter (
 wire rst;
   // All output pins must be assigned. If not used, assign to 0.
   // assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
+    assign uio_out[5:0] = 0;
   assign uio_oe[4:0]  = 0;
   assign uio_oe[6:5]  = 1;
   assign uio_oe[7]  = 0;
